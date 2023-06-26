1e9fe90e6853adb1fb905066e0367d6d70d5c7df
Somtochukwu Nnaji <lyon4197@gmail.com>
2023-06-26 18:42:47 +0100

    Added description to the image

--- src/Features/DashboardFeatures/SlideContent.tsx
+++ src/Features/DashboardFeatures/SlideContent.tsx
@@ -1,17 +1,24 @@
+import { ReactNode } from "react";
+
 type PropType = {
 	src: string;
 	alt: string;
+	children: ReactNode;
 };
 
-export const SlideContent = ({ src, alt }: PropType) => {
+export const SlideContent = ({ src, alt, children }: PropType) => {
 	return (
 		<div>
-			<div className="h-[18.4rem] w-full">
+			<div className="h-[18.4rem] w-full relative">
 				<img
 					src={src}
 					alt={alt}
-					className="h-full w-full object-cover rounded"
+					className=" h-full w-full object-cover rounded"
 				/>
+
+				<div className="bg-gradient-to-r from-black  absolute top-0 z-30 w-1/2  h-full">
+					{children}
+				</div>
 			</div>
 		</div>
 	);
