77be5b2d6153a1dff5f60057ec6c3fe72e7c1b09
Praiz Lightbringer <102701625+Somtochukwu21@users.noreply.github.com>
2023-06-27 01:30:57 +0100

    Revert "Nothing Much"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
-	theme: {
-		extend: {
-			screens: {
-				sm: "320px",
-			},
-		},
-	},
-	plugins: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "320px",
+      },
+      
+    },
+  },
+  plugins: [],
 };
+
